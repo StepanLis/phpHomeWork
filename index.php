2a485ad2faaace4e2272214da44d173bef63d982
fet1sov <august9222@gmail.com>
2023-10-14 09:04:19 +0500

    Task 2: Database Implementation
    Just implemented database into project :)

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-class ShopProduct 
+abstract class ShopProduct 
 {
 
     function __construct(private string $title, private float $weight, private int $amount, private float $price) 
@@ -12,7 +12,7 @@ class ShopProduct
         return $this->title;
     }
 
-    public function setTitle(string $new_title)
+    public function setTitle(string $new_title) : void
     {
         $this->title = $new_title;
     }
@@ -22,7 +22,7 @@ class ShopProduct
         return $this->weight;
     }
 
-    public function setWeight(float $new_weight)
+    public function setWeight(float $new_weight) : void
     {
         $this->weight = $new_weight;
     }
@@ -32,7 +32,7 @@ class ShopProduct
         return $this->amount;
     }
 
-    public function setAmount(int $new_amount)
+    public function setAmount(int $new_amount)  : void
     {
         $this->amount = $new_amount;
     }
@@ -42,9 +42,51 @@ class ShopProduct
         return $this->price;
     }
 
-    public function setPrice(int $new_price)
+    public function setPrice(float $new_price) : void
     {
         $this->price = $new_price;
+    }
+
+    
+
+    abstract public function actionCreate(PDO $mysql) : void;
+
+    public static function getInstance(int $id, PDO $mysql)
+    {
+        $stmt = $mysql->prepare("SELECT * FROM `products` WHERE id=?");
+        $stmt->execute([$id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!empty($row))
+        {
+            return null;
+        }
+
+        if ($row["product_type"] === "cd")
+        {
+            return new CDProduct(
+                $row["title"],
+                (float) $row["weight"],
+                (int) $row["amount"],
+                (float) $row["price"],
+                $row["cover_link"],
+                (float) $row["length"],
+                $row["type"],
+                $row["author"]
+            );
+        } elseif ($row["product_type"] === "book") {
+            return new BookProduct(
+                $row["title"],
+                (float) $row["weight"],
+                (int) $row["amount"],
+                (float) $row["price"],
+                $row["author"],
+                (int) $row["page_amount"],
+                $row["serial_number"],
+                $row["rating"],
+                $row["genre"]
+            );
+        }
     }
 }
 
@@ -76,7 +118,7 @@ class CDProduct extends ShopProduct
         return $this->cover_link;
     }
 
-    public function setCoverLink(string $new_cover_link) 
+    public function setCoverLink(string $new_cover_link) : void
     {
         $this->cover_link = $new_cover_link;
     }
@@ -86,7 +128,7 @@ class CDProduct extends ShopProduct
         return $this->length;
     }
 
-    public function setLength(float $new_length) 
+    public function setLength(float $new_length) : void 
     {
         $this->length = $new_length;
     }
@@ -96,7 +138,7 @@ class CDProduct extends ShopProduct
         return $this->type;
     }
 
-    public function setType(string $new_type) 
+    public function setType(string $new_type) : void 
     {
         $this->type = $new_type;
     }
@@ -106,9 +148,22 @@ class CDProduct extends ShopProduct
         return $this->author;
     }
 
-    public function setAuthor(int $new_author) 
+    public function setAuthor(int $new_author) : void 
     {
         $this->author = $new_author;
+    }
+
+    public function actionCreate(PDO $mysql) : void
+    {
+        $stmt = $mysql->prepare("INSERT INTO `products` (`title`, `weight`, `amount`, `price`, `product_type`)
+        VALUES (?, ?, ?, ?, ?)
+        ");
+        $stmt->execute([$this->getTitle(), $this->getWeight(), $this->getAmount(), $this->getPrice(), "cd"]);
+
+        $stmt = $mysql->prepare("INSERT INTO `cdProducts` (`product_id`, `cover_link`, `length`, `type`, `author`)
+        VALUES (?, ?, ?, ?, ?)
+        ");
+        $stmt->execute([$mysql->lastInsertId(), $this->cover_link, $this->length, $this->type, $this->author]);
     }
 }
 
@@ -142,7 +197,7 @@ class BookProduct extends ShopProduct
         return $this->author;
     }
 
-    public function setAuthor(string $new_author)
+    public function setAuthor(string $new_author) : void
     {
         $this->author = $new_author;
     }
@@ -152,7 +207,7 @@ class BookProduct extends ShopProduct
         return $this->page_amount;
     }
 
-    public function setPageAmount(int $new_page_amount)
+    public function setPageAmount(int $new_page_amount) : void
     {
         $this->page_amount = $new_page_amount;
     }
@@ -162,7 +217,7 @@ class BookProduct extends ShopProduct
         return $this->serial_number;
     }
 
-    public function setSerialNumber(string $new_srn)
+    public function setSerialNumber(string $new_srn) : void
     {
         $this->serial_number = $new_srn;
     }
@@ -172,7 +227,7 @@ class BookProduct extends ShopProduct
         return $this->rating;
     }
 
-    public function setRating(string $new_srn)
+    public function setRating(string $new_srn) : void
     {
         $this->serial_number = $new_srn;
     }
@@ -182,45 +237,95 @@ class BookProduct extends ShopProduct
         return $this->genre;
     }
 
-    public function setGenre(string $new_genre)
+    public function setGenre(string $new_genre) : void
     {
         $this->genre = $new_genre;
     }
-}
 
-class Program {
-    public function showProductInfo(ShopProduct $shopProduct) 
+    public function actionCreate(PDO $mysql) : void
     {
-        $string = "ShopProduct <br> Title: {$shopProduct->getTitle()} <br> Weight: {$shopProduct->getWeight()} kg <br> Amount: {$shopProduct->getAmount()} <br> Price: {$shopProduct->getPrice()} <br> <br>";
-        print($string);
-    }
+        $stmt = $mysql->prepare("INSERT INTO `products` (`title`, `weight`, `amount`, `price`, `product_type`)
+        VALUES (?, ?, ?, ?, ?)
+        ");
+        $stmt->execute([$this->getTitle(), $this->getWeight(), $this->getAmount(), $this->getPrice(), "book"]);
 
-    public function showCDProduct(CDProduct $cdProduct) 
-    {
-        $string = "CDProduct <br> 
-        Title: {$cdProduct->getTitle()} <br> Weight: {$cdProduct->getWeight()} kg <br> Amount: {$cdProduct->getAmount()} <br> Price: {$cdProduct->getPrice()} <br> <br>
-        Cover Link: {$cdProduct->getCoverLink()} <br> Length: {$cdProduct->getLength()} <br> Type: {$cdProduct->getTitle()} <br> Author: {$cdProduct->getAuthor()}
-        <br> <br>";
-        print($string);
-    }
-
-    public function showBookProduct(BookProduct $bookProduct) 
-    {
-        $string = "BookProduct <br> 
-        Title: {$bookProduct->getTitle()} <br> Weight: {$bookProduct->getWeight()} kg <br> Amount: {$bookProduct->getAmount()} <br> Price: {$bookProduct->getPrice()} <br> <br>
-        Author: {$bookProduct->getAuthor()} <br> Genre: {$bookProduct->getGenre()} <br> Page Amount: {$bookProduct->getPageAmount()} <br> Rating: {$bookProduct->getRating()} <br> SRN: {$bookProduct->getSerialNumber()}
-        <br> <br>";
-        print($string);
+        $stmt = $mysql->prepare("INSERT INTO `bookProducts` (`product_id`, `author`, `page_amount`, `serial_number`, `rating`, `genre`)
+        VALUES (?, ?, ?, ?, ?, ?)
+        ");
+        $stmt->execute([$mysql->lastInsertId(), $this->author, $this->page_amount, $this->serial_number, $this->rating, $this->genre]);
     }
 }
 
-$page = new Program();
+class WebSite {
 
-$shopProduct = new ShopProduct("Test Product", 15.0, 1, 15000.23);
-$page->showProductInfo($shopProduct);
+    public function createConnection(string $dbName, string $dbLogin, string $dbPass) : PDO
+    {
+        $dsn = "mysql:host=localhost;dbname=" . $dbName . ";charset=UTF8";
+        $pdo = new PDO($dsn, $dbLogin, $dbPass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $pdo->query("
+        CREATE TABLE IF NOT EXISTS `products` (
+            `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+            `title` VARCHAR(255) NOT NULL,
+            `weight` float(255,2) NOT NULL,
+            `amount` int(255) NOT NULL,
+            `price` float(255,2) NOT NULL,
+            `product_type` VARCHAR(255)
+        );
+        
+        CREATE TABLE IF NOT EXISTS `cdProducts` (
+            `product_id` INT,
+            `cover_link` VARCHAR(255) NOT NULL,
+            `length` float(15,2) NOT NULL,
+            `type` VARCHAR(255) NOT NULL,
+            `author` VARCHAR(255) NOT NULL,
+               
+            FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE CASCADE
+        );
+
+        CREATE TABLE IF NOT EXISTS `bookProducts` (
+            `product_id` INT,
+            `author` VARCHAR(255) NOT NULL,
+            `page_amount` INT(20) NOT NULL,
+            `serial_number` VARCHAR(10) NOT NULL,
+            `rating` INT(2) NOT NULL,
+            `genre` VARCHAR(10) NOT NULL,
+                       
+            FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE CASCADE
+        );
+        ");
+
+        return $pdo;
+    }
+
+    public function showProductInfo(mixed $product) : void
+    {
+        $string = "No product found <br>";
+
+        if ($product instanceof CDProduct) {
+            $string = "CDProduct <br> 
+            Title: {$product->getTitle()} <br> Weight: {$product->getWeight()} kg <br> Amount: {$product->getAmount()} <br> Price: {$product->getPrice()} <br> <br>
+            Cover Link: {$product->getCoverLink()} <br> Length: {$product->getLength()} <br> Type: {$product->getTitle()} <br> Author: {$product->getAuthor()}
+            <br> <br>";
+        } elseif ($product instanceof BookProduct) {
+            $string = "BookProduct <br> 
+            Title: {$product->getTitle()} <br> Weight: {$product->getWeight()} kg <br> Amount: {$product->getAmount()} <br> Price: {$product->getPrice()} <br> <br>
+            Author: {$product->getAuthor()} <br> Genre: {$product->getGenre()} <br> Page Amount: {$product->getPageAmount()} <br> Rating: {$product->getRating()} <br> SRN: {$product->getSerialNumber()}
+            <br> <br>";
+        }
+
+        print($string);
+    }
+}
+
+$page = new WebSite();
+$mysql = $page->createConnection("arhzfxgj_m4", "arhzfxgj", "Ju5M96");
 
 $cdProduct = new CDProduct("The End", 15.0, 1, 307827.84, "img/test.jpg", 15.23, "CD-RW", "Testoviy Chelovek");
-$page->showCDProduct($cdProduct);
+$cdProduct->actionCreate($mysql);
+$page->showProductInfo($cdProduct);
 
 $bookProduct = new BookProduct("Testovaya Knishka", 15.0, 1, 1231.41, "Lousville", 22, "YBSNW-2823", 5, "Drama");
-$page->showBookProduct($bookProduct);
+$bookProduct->actionCreate($mysql);
+$page->showProductInfo($bookProduct);
